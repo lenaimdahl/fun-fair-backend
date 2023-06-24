51843e3d628c895f88e862ef9ade0bd86e4cd85b
Lena Imdahl <mail@lenaimdahl.de>
2023-06-24 12:11:59 +0200

    added Mood.model

---
+++ models/Mood.model.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const moodSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  title: {
+    type: String,
+    enum: ["happy", "sad", "angry", "in love", "sleepy"],
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Mood = model("Mood", moodSchema);
+
+module.exports = Mood;
+
+// '😊' (happy), '😔' (sad), '😡' (angry), '😍' (in love), and '😴' (sleepy).
