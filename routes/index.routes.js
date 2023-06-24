ca4ad176d6c14eff0e943ff85b09ad9f9bc40f03
Lena Imdahl <mail@lenaimdahl.de>
2023-06-24 12:07:32 +0200

    started api mood route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,29 @@
 const router = require("express").Router();
+const MoodModel = require("../models/Mood.model");
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
+
+router.get("/api/mood", async (req, res) => {
+  try {
+    console.log(req.payload);
+    // const allMoods = await MoodModel.
+  } catch (error) {
+    console.log("There was an error", error);
+  }
+});
+
+// Mood;
+
+// router.postMood;
+
+// router.getActivity;
+
+// router.postActivity;
+
+// router.getDrink;
+
+// router.postDrink;
 
 module.exports = router;
