b01cdd59a836ad0ca5985dfb87893f40965c43de
Lena Imdahl <mail@lenaimdahl.de>
2023-07-01 16:03:17 +0200

    added Text.model

---
+++ models/Text.model.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const textSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Text = model("text", textSchema);
+
+module.exports = Text;
