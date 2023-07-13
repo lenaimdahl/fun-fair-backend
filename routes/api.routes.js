d30d0bd4806424599298b91d575b6988b3712212
Lena Imdahl <mail@lenaimdahl.de>
2023-07-13 21:00:03 +0200

    fetched username of friends

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -24,7 +24,13 @@ router.get("/friends", async (req, res) => {
   try {
     const ownUserId = req.payload._id;
     const { friends: myFriends } = await UserModel.findById(ownUserId);
-    res.status(200).json({ friends: myFriends });
+    // Fetches usernames of all friends
+    const friendsData = await UserModel.find(
+      { _id: { $in: myFriends } },
+      "username"
+    );
+
+    res.status(200).json({ friends: friendsData });
   } catch (err) {
     console.error("ERROR while fetching all user from db :>>", err);
     res.status(500).json({ message: "Internal Server Error" });
